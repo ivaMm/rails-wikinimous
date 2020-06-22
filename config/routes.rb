ff4e681a089cbd014c1b886a9ea88d4bf0e2a238
iva mudronja <iva.pilizota@gmail.com>
2020-06-22 16:12:25 +0200

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # resource :articles
-  get 'articles', to: 'articles#index'
-  get 'articles/new', to: 'articles#new', as: :new_article
-  get 'articles/:id', to: 'articles#show', as: :article
-  post 'articles', to: 'articles#create'
-  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
-  patch 'articles/:id', to: 'articles#update'
-  delete 'articles/:id', to: 'articles#destroy'
+  resources :articles
 end
